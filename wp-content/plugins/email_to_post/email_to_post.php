bd46fb059a5ab0f1352a446a11c67bcbbb8b91fd
bishalxrauniyar <Bishal.rauniyar@gmail.com>
2025-03-06 15:36:16 +0545

    done by date

--- wp-content/plugins/email_to_post/email_to_post.php
+++ wp-content/plugins/email_to_post/email_to_post.php
@@ -40,8 +40,10 @@ function etp_fetch_emails_callback()
     // creating a table to display the fetched emails
     echo '<table class="wp-list-table widefat fixed striped">';
     echo '<tr><th>From</th><th>Subject</th><th>Date</th><th>Message</th></tr>';
-    // get all the posts of type 'post'
-    $posts = get_posts(array('post_type' => 'post'));
+
+
+    // get all the posts of type 'post' by using get_posts() function which only 5 posts default so set it to -1 to get all the posts
+    $posts = get_posts(array('post_type' => 'post', 'numberposts' => -1));
     foreach ($posts as $post) {
         echo '<tr>';
         echo '<td>' . get_post_meta($post->ID, 'email_from', true) . '</td>';
@@ -56,20 +58,30 @@ function etp_fetch_emails_callback()
 
 function etp_fetch_emails()
 {
-
-    $hostname = '{wplocatepress.com:993/imap/ssl}INBOX'; //email hostname with port number and email protocol (IMAP) ssl
-    $username = 'pipetest@wplocatepress.com';
+    $hostname = '{wplocatepress.com:993/imap/ssl}INBOX';
+    $username = 'pipetest@wplocatepress.com'; // Use environment variable
     $password = 'k342+11$c1_';
 
-
-    //CONNECT TO EMAIL
+    // CONNECT TO EMAIL
     $email = imap_open($hostname, $username, $password) or die('Cannot connect to email: ' . imap_last_error());
-    //GET EMAILS
-    $emails = imap_search($email, 'UNSEEN'); //for unseen emails
-    var_dump($emails);
 
+    // GET EMAILS
+    $emails = imap_search($email, 'ALL');
 
+    // Fetch last post date
+    $args = array(
+        'post_type'      => 'post',
+        'orderby'        => 'ID',
+        'order'          => 'DESC',
+        'posts_per_page' => 1,
+    );
+    $query = new WP_Query($args);
+    $last_post_date = '2025-01-01 00:00:00';
 
+    if ($query->have_posts()) {
+        $last_post_date = get_the_date('Y-m-d H:i:s', $query->posts[0]->ID);
+    }
+    wp_reset_postdata();
 
     if ($emails) {
         foreach ($emails as $email_number) {
@@ -78,33 +90,48 @@ function etp_fetch_emails()
             $subject = $headerInfo->subject ?? 'No Subject';
             $date = date("Y-m-d H:i:s", strtotime($headerInfo->date ?? 'now'));
 
-            // Get email message
-            $message = imap_fetchbody($email, $email_number, 1);
+            if ($date <= $last_post_date) {
+                continue; // Skip emails older than the last post
+            }
+
+            // Get email message (handling multipart emails)
+            $structure = imap_fetchstructure($email, $email_number);
+            $message = '';
+
+            if (!empty($structure->parts)) {
+                foreach ($structure->parts as $part_index => $part) {
+                    if ($part->subtype == 'PLAIN') {
+                        $message = imap_fetchbody($email, $email_number, $part_index + 1);
+                        break;
+                    }
+                }
+            } else {
+                $message = imap_fetchbody($email, $email_number, 1);
+            }
+
             if (empty($message)) {
                 $message = imap_fetchbody($email, $email_number, 1.1);
             }
 
-            // // Debugging output
-            // echo "<h3>Email #$email_number</h3>";
-            // echo "<strong>From:</strong> $from <br>";
-            // echo "<strong>Subject:</strong> $subject <br>";
-            // echo "<strong>Date:</strong> $date <br>";
-            // echo "<strong>Message:</strong> <pre>" . htmlspecialchars($message) . "</pre><br><br>";
-
-            $post = array(
-                'post_title' => $subject,
+            // CREATE POST  
+            $post_id = wp_insert_post(array(
+                'post_title'   => $subject,
                 'post_content' => $message,
-                'post_date' => $date,
-                'post_status' => 'publish',
-                'post_author' => 1,
-                'post_type' => 'post'
-            );
-            $post_id = wp_insert_post($post);
-            //ADD POST META
-            add_post_meta($post_id, 'email_from', $from);
+                'post_date'    => $date,
+                'post_status'  => 'publish',
+                'post_author'  => 1,
+                'post_type'    => 'post'
+            ));
+
+            if ($post_id) {
+                add_post_meta($post_id, 'email_from', $from);
+                echo '<p>Email fetched and post created successfully.</p>';
+            } else {
+                echo '<p>Error creating post.</p>';
+            }
         }
     }
+
+    // CLOSE IMAP CONNECTION
+    imap_close($email);
 }
-
-
-//
