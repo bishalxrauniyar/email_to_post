bb25420503e11d001b897a600684490a98ad5ad2
bishalxrauniyar <Bishal.rauniyar@gmail.com>
2025-03-06 10:03:53 +0545

    post 2

--- wp-content/plugins/email_to_post/email_to_post.php
+++ wp-content/plugins/email_to_post/email_to_post.php
@@ -13,6 +13,45 @@
 // If this file is called directly, abort.
 if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
+function etp_menu()
+{
+    add_menu_page(
+        'Email To Post',
+        'Email To Post',
+        'manage_options',
+        'email-to-post',
+        'etp_fetch_emails_callback',
+        'dashicons-email-alt',
+    );
+}
+add_action('admin_menu', 'etp_menu');
+
+function etp_fetch_emails_callback()
+{
+    echo '<h1>Email To Post</h1>';
+    // creating a button to fetch emails
+    echo '<form method="post" action="">
+    <input type="submit" name="fetch_emails" value="Fetch Emails" class="button button-primary">
+    </form>';
+    // if the button is clicked
+    if (isset($_POST['fetch_emails']))
+        etp_fetch_emails();
+    echo '<p>Emails fetched and posts created successfully.</p>';
+    // creating a table to display the fetched emails
+    echo '<table class="wp-list-table widefat fixed striped">';
+    echo '<tr><th>From</th><th>Subject</th><th>Date</th><th>Message</th></tr>';
+    // get all the posts of type 'post'
+    $posts = get_posts(array('post_type' => 'post'));
+    foreach ($posts as $post) {
+        echo '<tr>';
+        echo '<td>' . get_post_meta($post->ID, 'email_from', true) . '</td>';
+        echo '<td>' . $post->post_title . '</td>';
+        echo '<td>' . $post->post_date . '</td>';
+        echo '<td>' . $post->post_content . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+}
 
 
 function etp_fetch_emails()
@@ -21,6 +60,7 @@ function etp_fetch_emails()
     $hostname = '{wplocatepress.com:993/imap/ssl}INBOX';
     $username = 'pipetest@wplocatepress.com';
     $password = '*1j?4(+l2#:x';
+
 
     //CONNECT TO EMAIL
     $email = imap_open($hostname, $username, $password) or die('Cannot connect to email: ' . imap_last_error());
@@ -52,5 +92,6 @@ function etp_fetch_emails()
         }
     }
 }
+
 
 //
